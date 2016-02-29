bfef224aebe700025e99b936f5fed21147d30d2c
Andrew MacDonald <andrew@maccas.net>
2016-02-29 18:31:01 +1100

    Add RMSE implementation

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,25 @@ pub extern fn mse_c (obs_pointer: *const f64,
     return mse( obs_arr, sim_arr);
 }
 
+#[no_mangle]
+pub extern fn rmse_c (obs_pointer: *const f64,
+    sim_pointer: *const f64, arr_size: usize
+) -> f64 {
+
+    let obs_arr = unsafe {
+            std::slice::from_raw_parts(
+                obs_pointer as *const f64,
+                arr_size as usize)
+    };
+    let sim_arr = unsafe {
+            std::slice::from_raw_parts(
+                sim_pointer as *const f64,
+                arr_size as usize)
+    };
+
+    return rmse( obs_arr, sim_arr);
+}
+
 fn mse(obs: &[f64], sim: &[f64]) -> f64 {
     let mut sum_y: f64 = 0.0;
 
@@ -28,6 +47,43 @@ fn mse(obs: &[f64], sim: &[f64]) -> f64 {
     }
     let len: f64 = obs.len() as f64;
     return sum_y / len;
+}
+
+fn rmse(obs: &[f64], sim: &[f64]) -> f64 {
+    return mse(obs, sim).sqrt();
+}
+
+#[test]
+fn test_rmse() {
+    let test_obs = &[13.,17.,18.,20.,24.];
+    let test_sim = &[12.,15.,20.,22.,24.];
+
+    let result = rmse(test_obs, test_sim);
+    assert_eq!(result, (2.6 as f64).sqrt());
+}
+
+#[test]
+fn test_rmse_perfect() {
+    let test_obs = &[1.,2.,3.,4.,5.];
+    let test_sim = &[1.,2.,3.,4.,5.];
+
+    let result = rmse(test_obs, test_sim);
+    assert_eq!(result, (0 as f64));
+}
+
+#[test]
+fn test_rmse_bad() {
+    let obs: [f64; 5] = [1.,2.,3.,4.,5.];
+
+    let sum: f64 = obs.iter().fold((0 as f64), std::ops::Add::add);
+    let m = sum / (obs.len() as f64);
+    let sim = [m, m, m, m, m];
+    let result = rmse(
+        &[1.,2.,3.,4.,5.],
+        &sim
+    );
+
+    assert_eq!(result, (2.0 as f64).sqrt());
 }
 
 #[test]
