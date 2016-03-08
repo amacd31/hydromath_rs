a54a2dc0ad344155e0812d9fe1ed7bf4130bb2d2
Andrew MacDonald <andrew@maccas.net>
2016-03-08 18:46:11 +1100

    Add nse_c to C header file

--- hydromath.h
+++ hydromath.h
@@ -1,2 +1,3 @@
 extern double mse_c(double * obs, double * sim, int arr_len);
 extern double rmse_c(double * obs, double * sim, int arr_len);
+extern double nse_c(double * obs, double * sim, int arr_len);
