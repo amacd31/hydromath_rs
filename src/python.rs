16b461d219f58d2a8eee5a63044c534381c5395c
Andrew MacDonald <andrew@maccas.net>
2023-09-02 20:46:48 +1000

    Replace Python API wrapper funcs with macro
    Reduces code duplication.

--- src/python.rs
+++ src/python.rs
@@ -4,73 +4,31 @@ use pyo3::prelude::*;
 
 use crate::metrics;
 
-#[pyfunction]
-pub fn mse(obs: PyReadonlyArray1<f64>, sim: PyReadonlyArray1<f64>) -> PyResult<f64> {
-    let obs = match obs.as_slice() {
-        Ok(obs) => obs,
-        Err(error) => return Err(PyTypeError::new_err(error)),
+macro_rules! create_python_numpy_function {
+    ($func_name:ident) => {
+        #[pyfunction]
+        pub fn $func_name(obs: PyReadonlyArray1<f64>, sim: PyReadonlyArray1<f64>) -> PyResult<f64> {
+            let obs = match obs.as_slice() {
+                Ok(obs) => obs,
+                Err(error) => return Err(PyTypeError::new_err(error)),
+            };
+
+            let sim = match sim.as_slice() {
+                Ok(sim) => sim,
+                Err(error) => return Err(PyTypeError::new_err(error)),
+            };
+
+            let skill = metrics::$func_name(obs, sim);
+
+            Ok(skill)
+        }
     };
-
-    let sim = match sim.as_slice() {
-        Ok(sim) => sim,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let skill = metrics::mse(obs, sim);
-
-    Ok(skill)
 }
 
-#[pyfunction]
-pub fn rmse(obs: PyReadonlyArray1<f64>, sim: PyReadonlyArray1<f64>) -> PyResult<f64> {
-    let obs = match obs.as_slice() {
-        Ok(obs) => obs,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let sim = match sim.as_slice() {
-        Ok(sim) => sim,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let skill = metrics::rmse(obs, sim);
-
-    Ok(skill)
-}
-
-#[pyfunction]
-pub fn nse(obs: PyReadonlyArray1<f64>, sim: PyReadonlyArray1<f64>) -> PyResult<f64> {
-    let obs = match obs.as_slice() {
-        Ok(obs) => obs,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let sim = match sim.as_slice() {
-        Ok(sim) => sim,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let skill = metrics::nse(obs, sim);
-
-    Ok(skill)
-}
-
-#[pyfunction]
-pub fn kge(obs: PyReadonlyArray1<f64>, sim: PyReadonlyArray1<f64>) -> PyResult<f64> {
-    let obs = match obs.as_slice() {
-        Ok(obs) => obs,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let sim = match sim.as_slice() {
-        Ok(sim) => sim,
-        Err(error) => return Err(PyTypeError::new_err(error)),
-    };
-
-    let skill = metrics::kge(obs, sim);
-
-    Ok(skill)
-}
+create_python_numpy_function!(mse);
+create_python_numpy_function!(rmse);
+create_python_numpy_function!(nse);
+create_python_numpy_function!(kge);
 
 #[pymodule]
 pub fn hydromath(_py: Python, m: &PyModule) -> PyResult<()> {
